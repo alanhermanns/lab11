09e55107fde4e00138f1d29084e20f037f5d71e3
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-12 15:45:29 -0800

    small minor things

--- public/listitemyoubetterrecognize.js
+++ public/listitemyoubetterrecognize.js
@@ -1,14 +1,23 @@
 import Component from './component.js';
 
 export class ListItem extends Component {
+    onRender(dom){
+        const doneButton = dom.querySelector('.done');
+        const oneToDo = dom.querySelector('.thing-to-do');
+        doneButton.addEventListener('click', () => {
+            oneToDo.classList.addClass('hidden');
+        });            
+        
+    }
+
 
     renderHTML(){
         const itemData = this.props;
         const dom = /*html*/`
-        <div> 
-            <li>I have to:${itemData.name}</li>
-            <li>I got to how?${itemData.body}</li>
-            <li>Have I?${itemData.done}</li>
+        <div class ='thing-to-do'> 
+           <button class='done'>It's Done.</button> <li>I have to ${itemData.name}</li>
+           <li>I got to how? ${itemData.body}</li>
+           <li>Have I? ${itemData.done}</li>
         </div>
         `;
         return dom;
