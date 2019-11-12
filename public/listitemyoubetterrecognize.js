6b1e645d6c118c3f31f8b149c56f2bb12c44973a
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-12 11:46:10 -0800

    small adjust prps

--- public/listitemyoubetterrecognize.js
+++ public/listitemyoubetterrecognize.js
@@ -2,7 +2,8 @@ import Component from './component.js';
 
 export class ListItem extends Component {
 
-    renderHTML(itemData){
+    renderHTML(){
+        const itemData = this.props;
         const dom = /*html*/`    
         <li>${itemData.name}</li>
         <li>${itemData.body}</li>
--- public/todolistyoubetterrecognize.js
+++ public/todolistyoubetterrecognize.js
@@ -3,8 +3,9 @@ import { ListItem } from './listitemyoubetterrecognize.js';
 
 class ToDoList extends Component {
     onRender(dom){
-        const theList = document.querySelector('.list');
+        const theList = dom.querySelector('.list');
         this.props.forEach((item) => {
+            console.log(item);
             const newListItem = new ListItem(item);
             const newListItemDOM = newListItem.renderDOM();
             theList.appendChild(newListItemDOM);
